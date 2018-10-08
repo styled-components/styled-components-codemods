b4579c6b633a5709ee712c6a4565b11bfe0aaf3f
Steven Langbroek <steven@digitaaldier.nl>
2018-10-08 14:59:25 +0200

    Minor cleanup

--- src/v4/injectGlobalToCreateGlobalStyle.js
+++ src/v4/injectGlobalToCreateGlobalStyle.js
@@ -1,17 +1,18 @@
 "use strict";
 
 const t = require("babel-core").types;
-const { prop } = require("lodash/fp");
+const { prop, set } = require("lodash/fp");
 const { manipulateOptions } = require("./common");
 
 const WARNING =
   "An injectGlobal usage was converted to createGlobalStyles via codemod but needs to be hooked up. See styled-components.com/docs/api#createglobalstyle for instructions.";
-
 const PREVIOUS = "injectGlobalStyle";
 const NEXT = "createGlobalStyle";
+const COMPONENT_ID = t.identifier("GlobalStyle");
+const NEXT_ID = t.identifier(NEXT);
 
 const isInjectGlobalCall = path =>
-  prop("node.expression.tag.name")(path) === "injectGlobalStyle";
+  prop("node.expression.tag.name")(path) === PREVIOUS;
 
 let shouldAddWarning = false;
 
@@ -39,6 +40,7 @@ module.exports = () => ({
       if (prop("node.source.value")(path) !== "styled-components") {
         return;
       }
+
       const injectGlobalImport = path.node.specifiers.find(specifier => {
         if (t.isImportDefaultSpecifier(specifier)) {
           return false;
@@ -50,26 +52,26 @@ module.exports = () => ({
         return;
       }
 
+      // Remove existing named import
       path.node.specifiers.splice(
         path.node.specifiers.indexOf(injectGlobalImport),
         1
       );
 
-      path.node.specifiers.push(
-        t.importSpecifier(t.identifier(NEXT), t.identifier(NEXT))
-      );
+      // Push new one in.
+      path.node.specifiers.push(t.importSpecifier(NEXT_ID, NEXT_ID));
     },
     ExpressionStatement(path) {
       if (!isInjectGlobalCall(path)) {
         return;
       }
 
-      const createExpression = path.node.expression;
-      createExpression.tag.name = NEXT;
-      const id = path.scope.generateUidIdentifierBasedOnNode(path.node.id);
+      const expression = prop("node.expression")(path);
+
+      // Wrap existing TemplateExpression with a Variable Declaration, update its name.
       path.replaceWith(
         t.variableDeclaration("const", [
-          t.variableDeclarator(t.identifier("GlobalStyle"), createExpression)
+          t.variableDeclarator(COMPONENT_ID, set("tag.name", NEXT)(expression))
         ])
       );
     }
