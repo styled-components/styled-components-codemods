5fbd74fd92ee7dca27a83ddf6064fb1593473c22
Evan Scott <probablyup@gmail.com>
2018-10-10 12:54:24 -0500

    add globbing, make the terminal output nicer

--- bin/utils.js
+++ bin/utils.js
@@ -1,31 +1,34 @@
 'use strict';
 
 const { spawnSync } = require('child_process');
+const glob = require('fast-glob');
 const { curry } = require('lodash/fp');
 const path = require('path');
 
 const runCodemod = curry((version, files, mod) => {
-  // Account for both splat & single file
-  files = files.filter(f => typeof f === 'string');
-
   const pluginPath = path.resolve(path.join(__dirname, '..', 'src', version, `${mod}.js`));
-  console.log(`Running ${mod} at ${pluginPath} on ${files.length} files.`);
-
-  if (typeof files !== 'string' && !Array.isArray(files)) {
-    throw new Error('No files provided');
-  }
 
   spawnSync(path.resolve(path.join(__dirname, '../node_modules/.bin/codemod')), [
     '--plugin',
     pluginPath,
     ...files,
   ]);
-  console.log(`Finished running ${mod}`);
 });
 
 const runCodemods = (version, mods) => {
   return (...files) => {
+    files = glob.sync(files.filter(f => typeof f === 'string'));
+
+    if (typeof files !== 'string' && !Array.isArray(files)) {
+      throw new Error('No files provided.');
+    }
+
+    console.log(`\nRunning the ${mods.join(', ')} codemods against the following files:\n`);
+    console.log(files);
+
     mods.forEach(runCodemod(version, files));
+
+    console.log('\nDone. Enjoy!\n');
   };
 };
 
